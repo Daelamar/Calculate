a08eb8d8fe86b2ddc26ed182ebcf0c90b26f731b
Daelamar <redfess@yandex.ru>
2020-02-20 14:54:12 +0300

    Третье задание

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const DAY_STRING = ['день', 'дня', 'дней'];
+
 const DATA = {
     whichSite: ['landing', 'multiPage', 'onlineStore'],
     price: [4000, 8000, 26000],
@@ -23,7 +26,18 @@ const startButtonElement = document.querySelector('.start-button'),
     totalFieldsetElement = document.querySelector('.total'),
     fastRangeElement = document.querySelector('.fast-range'),
     totalPriceSumElement = document.querySelector('.total_price__sum'),
-    mobileTemplatesInputElement = document.querySelector('#mobileTemplates');
+    mobileTemplatesInputElement = document.getElementById('mobileTemplates'),
+    adaptInputElement = document.getElementById('adapt'),
+    typeOfSiteElement = document.querySelector('.type-site'),
+    maxDeadlineDaysElement = document.querySelector('.max-deadline'),
+    rangeDeadlineElement = document.querySelector('.range-deadline'),
+    deadlineValueElement = document.querySelector('.deadline-value'),
+    checkboxLabels = document.querySelectorAll('.checkbox-label');
+
+let declOfNum = function (n, titles) {
+    return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+        0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+};
 
 let showElement = function (element) {
     element.style.display = 'block';
@@ -33,10 +47,22 @@ let hideElement = function (element) {
     element.style.display = 'none';
 };
 
+let renderPageTextContent = function (site, total, maxDays, minDays) {
+    typeOfSiteElement.textContent = site;
+    totalPriceSumElement.textContent = total;
+    maxDeadlineDaysElement.textContent = declOfNum(maxDays, DAY_STRING);
+    rangeDeadlineElement.min = minDays;
+    rangeDeadlineElement.max = maxDays;
+    deadlineValueElement.textContent = declOfNum(rangeDeadlineElement.value, DAY_STRING);
+};
+
 let priceCalculation = function (element) {
     let result = 0,
         index,
-        options = [];
+        options = [],
+        dataSite = '',
+        maxDays,
+        minDays;
 
     if (element.name === 'whichSite') {
         for (let item of formInputElements) {
@@ -47,20 +73,34 @@ let priceCalculation = function (element) {
         hideElement(fastRangeElement);
     }
 
+
     for (let item of formInputElements) {
         if (item.name === 'whichSite' && item.checked) {
             index = DATA.whichSite.indexOf(item.value);
+            dataSite = item.dataset.site;
+            maxDays = DATA.deadlineDays[index][1];
+            minDays = DATA.deadlineDays[index][0]
         } else if (item.classList.contains('calc-handler') && item.checked) {
             options.push(item.value);
         }
     }
 
     for (let item of formInputElements) {
-        if (item.value === 'adapt' && item.checked) {
+        if (adaptInputElement.checked) {
             mobileTemplatesInputElement.removeAttribute('disabled');
-
-        } else if (item.value === 'adapt' && !item.checked) {
+        } else {
+            mobileTemplatesInputElement.checked = false;
             mobileTemplatesInputElement.setAttribute('disabled', 'disabled');
+        }
+    }
+
+    for (let item of formInputElements) {
+        for (let span of checkboxLabels) {
+            if ((span.classList.contains(item.value + '_value')) && item.checked) {
+                span.textContent = 'Да';
+            } else if ((span.classList.contains(item.value + '_value')) && !item.checked) {
+                span.textContent = 'Нет';
+            }
         }
     }
 
@@ -82,7 +122,7 @@ let priceCalculation = function (element) {
 
     result += DATA.price[index];
 
-    totalPriceSumElement.textContent = result;
+    renderPageTextContent(dataSite, result, maxDays, minDays);
 };
 
 let callbackFormHandler = function (evt) {
